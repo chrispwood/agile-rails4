02ec07bb6fe1635ba7bb5cb7abf6b7ef9ba127e5
Chris <cwood@lgscout.com>
2014-04-15 00:38:23 -0400

    chatper 11 completed

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,13 +29,11 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-    # @line_item = @cart.line_items.build(product: product)
-    # @line_item = LineItem.new(line_item_params)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
-        # format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
