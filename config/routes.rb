dd24f8db4da600e88612b89bffc53f5624cbf27a
Chris <cwood@lgscout.com>
2014-04-23 09:09:34 -0400

    Iteration J - I18n

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,7 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
 
-  get 'store/index'
   resources :products do
     get :who_bought, on: :member
   end
@@ -23,8 +19,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'store#index', as: 'store'
-  root :to =>'store#index'
+  # root :to =>'store#index'
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+    root :to =>'store#index'
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
